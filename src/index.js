47ee9e98b2008c8595c7919f4ae1e13d2ebe4274
Oren.Efraim <oren.efraim@gmail.com>
2016-10-25 18:18:44 +0300

    * create base state for the application.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Oren.Efraim on 10/25/2016.
+ */
+import "babel-polyfill";
+import './view/index.html';
+
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: __dirname +'/app',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]",
+            },
+
+        ],
+
+    }
+};
